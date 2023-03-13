adfe3f799899b41936ecfe9b66970ad254331e79
m104 <m104ngc4594@sina.com>
2023-03-13 15:28:52 +0800

    If valid form data is submitted, the data is saved in our database; if the submitted form is incomplete, the API returns a 400

---
+++ migrations/20230307080542_create_subscriptions_table.sql
@@ -0,0 +1,9 @@
+-- migrations/20230307080542_create_subscriptions_table.sql
+-- Create Subscriptions Table
+CREATE TABLE subscriptions (
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    subscribed_at timestamptz NOT NULL
+);
